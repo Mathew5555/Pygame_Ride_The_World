a44f82c28f2f0ef950e7124b05880deab5eed76c
NikitaMulyar <ni***ol@mail.ru>
2023-01-31 00:28:32 +0300

    Version 0.1.8.3 - New music in menu

--- consts.py
+++ consts.py
@@ -4,4 +4,4 @@ FPS = 45
 FPS2 = 30
 WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 1500, 1000
 FONT = 'data/font.ttf'
-TRACKS = [MUSIC_DIR + 'chasm' + f'{i}.mp3' for i in range(1, 14)]
+TRACKS = [MUSIC_DIR + 'chasm' + f'{i}.mp3' for i in range(1, 16)]
