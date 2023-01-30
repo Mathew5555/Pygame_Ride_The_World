cf00e280515c293efa19624e3b0a677dc9baa451
NikitaMulyar <ni***ol@mail.ru>
2023-01-30 20:08:21 +0300

    Version 0.1.8 - You can choose costume for game out of your wardrobe

--- wardrobe_window.py
+++ wardrobe_window.py
@@ -1,6 +1,7 @@
 from funcs_backend import *
 import pygame
 from consts import *
+import sqlite3
 
 
 pygame.init()
@@ -10,15 +11,84 @@ RUNNING = True
 
 
 class Wardrobe:
-    def __init__(self, back='back.png'):
+    def __init__(self, back='back.png', bottom='wb_bg.png', left='previous.png', right='next.png',
+                 ok='check.png'):
+        self.con = sqlite3.connect('data/account_info.db')
+        self.cur = self.con.cursor()
         self.btn_back = pic(back, (20, 20))
+        self.bottom1 = pic(bottom, (1280, 20), add=(200, 50))
+        self.bottom2 = pic(bottom, (1280, 80), add=(200, 50))
+        self.player1 = self.cur.execute("SELECT skins FROM info WHERE id = 1").fetchall()[0][0].split(';')
+        self.player2 = self.cur.execute("SELECT skins FROM info WHERE id = 2").fetchall()[0][0].split(';')
+        self.maxx1 = len(self.player1)
+        self.maxx2 = len(self.player2)
+        self.ind = 0
+        self.curr_pl = 1
+
+        self.btn_left = pic(left, (300, 875), add=(100, 100))
+        self.btn_right = pic(right, (1100, 875), add=(100, 100))
+        self.btn_pick = pic(ok, (700, 875), add=(100, 100))
+
+        self.font = pygame.font.Font(FONT, 45)
 
     def render(self, screen):
+        bottom = pygame.Surface((1600, 500))
+        bottom.set_alpha(50)
+        bottom.fill((255, 0, 255))
+        screen.blit(bottom, (-50, 250))
+
+        screen.blit(*self.bottom1)
+        screen.blit(*self.bottom2)
+        line = self.font.render('Игрок 1', True, (255, 255, 255))
+        screen.blit(line, (1292, 17))
+        line = self.font.render('Игрок 2', True, (255, 255, 255))
+        screen.blit(line, (1292, 77))
         screen.blit(*self.btn_back)
+        screen.blit(*self.btn_right)
+        screen.blit(*self.btn_left)
+        screen.blit(*self.btn_pick)
+
+    def render1_wb(self):
+        cnt = 0
+        for i in range(self.ind - 3, self.ind):
+            screen.blit(*pic(self.player1[i], (200 + cnt * 400, 350), add=(300, 300)))
+            line = self.font.render(f'Костюм {i + len(self.player1)}', True, (255, 255, 255))
+            screen.blit(line, (200 + cnt * 400 + 50, 700))
+            cnt += 1
+
+    def render2_wb(self):
+        cnt = 0
+        for i in range(self.ind - 3, self.ind):
+            screen.blit(*pic(self.player2[i], (200 + cnt * 400, 350), add=(300, 300)))
+            line = self.font.render(f'Костюм {i + len(self.player2)}', True, (255, 255, 255))
+            screen.blit(line, (200 + cnt * 400 + 50, 700))
+            cnt += 1
+
+    def next(self):
+        self.ind += 1
+        self.ind = self.ind % 4
+
+    def previous(self):
+        self.ind -= 1
+        if self.ind < 0:
+            if self.curr_pl == 1:
+                self.ind = self.maxx1 - 1
+            else:
+                self.ind = self.maxx2 - 1
+
+    def pick(self):
+        if self.curr_pl == 1:
+            self.cur.execute(
+                f"UPDATE info SET curr_skin = '{self.player1[self.ind - 2]}' WHERE id = {self.curr_pl}")
+        else:
+            self.cur.execute(
+                f"UPDATE info SET curr_skin = '{self.player2[self.ind - 2]}' WHERE id = {self.curr_pl}")
+        self.con.commit()
 
     def back(self):
         global RUNNING
         RUNNING = False
+        self.con.close()
 
 
 def wb():
@@ -28,6 +98,8 @@ def wb():
     ind = 0
     RUNNING = True
     clock.tick(FPS2)
+    fl = False
+    timer = 0
     while RUNNING:
         screen.blit(FON[ind], (0, 0))
         for event in pygame.event.get():
@@ -38,7 +110,28 @@ def wb():
                 if wardrobe.btn_back[1].collidepoint(*mouse):
                     wardrobe.back()
                     break
+                if wardrobe.bottom1[1].collidepoint(*mouse):
+                    wardrobe.curr_pl = 1
+                if wardrobe.bottom2[1].collidepoint(*mouse):
+                    wardrobe.curr_pl = 2
+                if wardrobe.btn_right[1].collidepoint(*mouse):
+                    wardrobe.next()
+                if wardrobe.btn_pick[1].collidepoint(*mouse):
+                    wardrobe.pick()
+                if wardrobe.btn_left[1].collidepoint(*mouse):
+                    wardrobe.previous()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    wardrobe.next()
+                if event.key == pygame.K_LEFT:
+                    wardrobe.previous()
+                if event.key == pygame.K_BREAK:
+                    wardrobe.pick()
         wardrobe.render(screen)
+        if wardrobe.curr_pl == 1:
+            wardrobe.render1_wb()
+        else:
+            wardrobe.render2_wb()
         sound_level = float(
             open('data/sound.txt', mode='r', encoding='utf-8').readlines()[0].strip('\n'))
         check_busy(sound_level)
