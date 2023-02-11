7d9cae3daef9993f56d845cbabc7b51380bb24f7
NikitaMulyar <ni***ol@mail.ru>
2023-02-11 03:47:32 +0300

    Version 1.0.1 Pre-Beta - United: Main Menu ang Gameplay

--- playing_mode.py
+++ playing_mode.py
@@ -164,7 +164,7 @@ class Hero(pygame.sprite.Sprite):
 
     def move(self, keys, game_map):
         map_flag = 0
-        if MAP_NAME == "map_2.tmx":
+        if MAP_NAME == "map_2.tmx" or MAP_NAME == "map3.tmx":
             map_flag = 2
         flag = 0
         if self.button(keys, "left"):
@@ -300,9 +300,11 @@ class Hero(pygame.sprite.Sprite):
             self.fire_flag -= 1
 
     def hill(self, game_map):
-        hill_check = get_tile_properties(game_map.map, self.rect.midbottom[0], self.rect.centery, 1)
-        if hill_check["hill"] == 1 and self.health < self.health_bar.max_health:
-            self.health += 0.3
+        if not MAP_NAME == "map_2.tmx":
+            hill_check = get_tile_properties(game_map.map, self.rect.midbottom[0],
+                                             self.rect.centery, 1)
+            if hill_check["hill"] == 1 and self.health < self.health_bar.max_health:
+                self.health += 0.3
 
 
 class Bullet(pygame.sprite.Sprite):
@@ -319,7 +321,7 @@ class Bullet(pygame.sprite.Sprite):
 
     def update(self, map):
         map_flag = 0
-        if MAP_NAME == "map_2.tmx":
+        if MAP_NAME == "map_2.tmx" or MAP_NAME == "map3.tmx":
             map_flag = 2
         if self.direction == "left":
             left_tile = get_tile_properties(map.map, self.rect.midleft[0] - 3, self.rect.bottomleft[1], map_flag)
@@ -425,6 +427,8 @@ class Game:
                 self.loser = self.lose()
         if MAP_NAME == "map_2.tmx":
             self.map.render(screen, 1, 2, 3, 4, 5, 6)
+        elif MAP_NAME == "map3.tmx":
+            self.map.render(screen, 1, 2, 3, 4, 5, 6, 7)
         else:
             self.map.render(screen, 1, 3)
         if not game_over:
