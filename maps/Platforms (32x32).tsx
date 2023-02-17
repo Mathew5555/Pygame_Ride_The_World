f122219f53bc8ee83ce22909ba8342d083811d84
NikitaMulyar <117512834+NikitaMulyar@users.noreply.github.com>
2023-02-18 01:24:54 +0300

    Delete maps directory

--- maps/Platforms (32x32).tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Platforms (32x32)" tilewidth="32" tileheight="32" tilecount="24" columns="6">
- <image source="Platforms (32x32).png" width="192" height="128"/>
-</tileset>
